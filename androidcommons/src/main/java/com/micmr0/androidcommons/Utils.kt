0b045c99c92b3770e033f872a0c5ae37fda7f2cb
micmr0 <micm93@hotmail.com>
2025-10-03 18:10:28 +0200

    🔖 Release v1.2.7

--- androidcommons/src/main/java/com/micmr0/androidcommons/Utils.kt
+++ androidcommons/src/main/java/com/micmr0/androidcommons/Utils.kt
@@ -1,9 +1,11 @@
 package com.micmr0.androidcommons
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.widget.Toast
 import androidx.annotation.RequiresPermission
 import androidx.core.net.toUri
 import com.google.gson.Gson
@@ -13,48 +15,128 @@ import java.io.BufferedReader
 fun goToGooglePlay(context: Context) {
     val packageName = context.packageName
 
-    val intent = Intent(Intent.ACTION_VIEW, "market://details?id=$packageName".toUri())
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    val marketIntent = Intent(Intent.ACTION_VIEW, "market://details?id=$packageName".toUri()).apply {
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
 
-    if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
-    } else {
-        val webIntent = Intent(
-            Intent.ACTION_VIEW,
-            "https://play.google.com/store/apps/details?id=$packageName".toUri()
-        )
-        context.startActivity(webIntent)
+    try {
+        if (marketIntent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(marketIntent)
+        } else {
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                "https://play.google.com/store/apps/details?id=$packageName".toUri()
+            ).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            if (webIntent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(webIntent)
+            } else {
+                Toast.makeText(context,
+                    context.getString(R.string.open_google_play_no_app), Toast.LENGTH_LONG).show()
+            }
+        }
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(context,
+            context.getString(R.string.open_google_play_problem), Toast.LENGTH_LONG).show()
     }
 }
 
-fun showPrivacyPolicy(context: Context, privacyPolicyUrl : String) {
+fun showPrivacyPolicy(context: Context, privacyPolicyUrl: String) {
     val intent = Intent(Intent.ACTION_VIEW).apply {
         data = privacyPolicyUrl.toUri()
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     }
-    context.startActivity(intent)
+
+    try {
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        } else {
+            Toast.makeText(
+                context,
+                context.getString(R.string.privacy_policy_no_app),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(
+            context,
+            context.getString(R.string.privacy_policy_problem),
+            Toast.LENGTH_LONG
+        ).show()
+    }
 }
 
 fun shareApp(context: Context) {
     val packageName = context.packageName
     val link = "https://play.google.com/store/apps/details?id=$packageName"
 
-    val intent = Intent(Intent.ACTION_SEND)
-    intent.type = "text/plain"
-    intent.putExtra(Intent.EXTRA_TEXT, link)
-    context.startActivity(
-        Intent.createChooser(
-            intent,
-            context.getString(R.string.select_an_app_to_share)
-        )
-    )
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, link)
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+
+    try {
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(
+                Intent.createChooser(
+                    intent,
+                    context.getString(R.string.select_an_app_to_share)
+                ).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+            )
+        } else {
+            Toast.makeText(
+                context,
+                context.getString(R.string.share_app_no_app),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(
+            context,
+            context.getString(R.string.share_app_problem),
+            Toast.LENGTH_LONG
+        ).show()
+    }
 }
 
-fun showMoreApps(context: Context, developerName : String) {
-    val uri = "https://play.google.com/store/apps/developer?id=$developerName".toUri()
-    val intent = Intent(Intent.ACTION_VIEW, uri)
-    intent.setPackage("com.android.vending")
-    context.startActivity(intent)
+fun showMoreApps(context: Context, developerName: String) {
+    val marketUri = "https://play.google.com/store/apps/developer?id=$developerName".toUri()
 
+    val marketIntent = Intent(Intent.ACTION_VIEW, marketUri).apply {
+        setPackage("com.android.vending")
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+
+    try {
+        if (marketIntent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(marketIntent)
+        } else {
+            val webIntent = Intent(Intent.ACTION_VIEW, marketUri).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            if (webIntent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(webIntent)
+            } else {
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.more_apps_no_app),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(
+            context,
+            context.getString(R.string.more_apps_problem),
+            Toast.LENGTH_LONG
+        ).show()
+    }
 }
 
 inline fun <reified T> loadAndParseJson(context: Context, fileName: String): List<T> {
