856b8b08460b177ff2dfd384ad90820ab410bd95
micmr0 <micm93@hotmail.com>
2026-08-19 14:04:56 +0200

    🔖 Release v1.6.2

--- androidcommons/src/main/java/com/micmr0/androidcommons/Extensions.kt
+++ androidcommons/src/main/java/com/micmr0/androidcommons/Extensions.kt
@@ -1,8 +1,19 @@
 package com.micmr0.androidcommons
 
 import android.content.Context
+import android.content.res.Configuration
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import java.util.Locale
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
+
+fun Context.createLocalizedContext(language: String): Context {
+    val locale = Locale.forLanguageTag(language.replace("-r", "-"))
+
+    val configuration = Configuration(resources.configuration)
+    configuration.setLocale(locale)
+
+    return createConfigurationContext(configuration)
+}
